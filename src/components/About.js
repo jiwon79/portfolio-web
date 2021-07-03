023b7c0481168a2e72c89e6e8037be9d0e515277
jiwon79 <19-079@ksa.hs.kr>
2021-07-03 22:48:37 +0900

    feat: Add About component CSS

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,11 @@ import './About.css'
 export default function About() {
     return (
         <div className="about" id="about">
-            <p className="title">About</p>
-            <img className="profile" src={require('./../img/profile.jpg').default} alt="profile"/>
-            <ul>
+            <div className="about__title">
+                <h1>About</h1>
+            </div>
+            <img className="about__profile" src={require('./../img/profile.jpg').default} alt="profile"/>
+            <ul className="about__content">
                 <li>안녕하세요 1년차 개발자 이지원입니다.</li>
                 <li>제가 만든 서비스를 남들이 사용해 주는 것이 좋아 개발을 하고 있습니다.</li>
                 <li>Clean Code, 완성도 높은 UI를 고민하고 있습니다.</li>
