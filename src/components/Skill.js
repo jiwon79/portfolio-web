1efc6375561feccc03bf8be94ad48a7bdc58a6f3
jiwon79 <19-079@ksa.hs.kr>
2021-07-04 00:17:21 +0900

    feat: Add Skill component CSS

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -4,7 +4,36 @@ import './Skill.css'
 export default function Skill() {
     return (
         <div className="skill" id="skill">
-            skill
+            <div className="skill__title">
+                <h1>Skill</h1>
+            </div>
+            <div className="skill__content">
+                <p className="skillName">
+                    Front End
+                </p>
+                <ul className="skillList">
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                </ul>
+                <p className="skillName">
+                    Backend
+                </p>
+                <ul className="skillList">
+                    <li>PHP</li>
+                    <li>Git</li>
+                    <li>Vim, Visual Studio Code</li>
+                </ul>
+                <p className="skillName">
+                    DevOps
+                </p>
+                <ul className="skillList">
+                    <li>AWS EC2, Foute 53</li>
+                    <li>MySQL</li>
+                    <li>Tomcat</li>
+                </ul>
+            </div>
         </div>
     )
 }
